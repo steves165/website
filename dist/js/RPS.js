d815d5f07b279c85840b278775c9af9d8e775311
stephen skidmore <stephen.skidmore@virginholidays.com>
2014-07-17 17:34:32 +0100

    lo

--- dist/js/RPS.js
+++ dist/js/RPS.js
@@ -1,30 +1,30 @@
-﻿var randomNumber = 0;
+var randomNumber = 0;
+
+function Result(type, win, lose, draw) {
+  this.type = type;
+  this.results = [win, '<h3>Yay you WIN!</h3>', lose, '<h3>Oh dear you lose!!</h3>', draw, '<h3>It was a draw!!</h3>'];
+}
+
+var rock = new Result(/^Rock/, 3, 2, 1);
+var paper = new Result(/^Paper/, 1, 3, 2);
+var scissors = new Result(/^Scissors/, 2, 1, 3);
 
 function process(inp) {
-    	randomNumber = 1 + Math.random() * 3;
-	randomNumber = Math.round(randomNumber);
 
-    if (inp == "Rock" && randomNumber == 1) {
-        document.getElementById("result").innerHTML = "<h1>It was a Draw!!</h1>";
-    } else if (inp == "Rock" && randomNumber == 2) {
-        document.getElementById("result").innerHTML = "<h1>Oh Dear you lose!!</h1>";
-    } else if (inp == "Rock" && randomNumber == 3) {
-        document.getElementById("result").innerHTML = "<h1>Yay you WIN!!</h1>";
-    } else if (inp == "Paper" && randomNumber == 1) {
-        document.getElementById("result").innerHTML = "<h1>Yay you WIN!!</h1>";
-    } else if (inp == "Paper" && randomNumber == 2) {
-        document.getElementById("result").innerHTML = "<h1>It was a Draw!!</h1>";
-    } else if (inp == "Paper" && randomNumber == 3) {
-        document.getElementById("result").innerHTML = "<h1>Oh Dear you lose!!</h1>";
-    } else if (inp == "Scissors" && randomNumber == 1) {
-        document.getElementById("result").innerHTML = "<h1>Oh Dear you lose!!</h1>";
-    } else if (inp == "Scissors" && randomNumber == 2) {
-        document.getElementById("result").innerHTML = "<h1>Yay you WIN!!</h1>";
-    } else if (inp == "Scissors" && randomNumber == 3) {
-        document.getElementById("result").innerHTML = "<h1>It was a Draw!!</h1>";
-    }
+  var resultArea = $('#result');
+  randomNumber = Math.round(1 + Math.random() * 2);
+
+  if (rock.type.test(inp)) {
+    resultArea.html(rock.results[rock.results.indexOf(randomNumber) + 1]);
+  } else if (paper.type.test(inp)) {
+    resultArea.html(paper.results[paper.results.indexOf(randomNumber) + 1]);
+  } else if (scissors.type.test(inp)) {
+    resultArea.html(scissors.results[scissors.results.indexOf(randomNumber) + 1]);
+  } else {
+    console.error('something odd has happened');
+  }
 }
 
 function clr() {
-	document.getElementById("result").innerHTML = '&nbsp;';
+	resultArea.html('&nbsp');
 }
